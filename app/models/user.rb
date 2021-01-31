c57e0151dcf20e5519400cdb0f229dedc7799d29
KenChukanit <chonlasek.c@gmail.com>
2021-01-31 13:11:51 -0800

    add user and session controller and view fn test done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    validates :email,
-    format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 4, maximum: 50 }
+    # validates :email,
+    # format: { with: /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/ , message: "Email invalid"  },
+    #         uniqueness: { case_sensitive: false },
+    #         length: { minimum: 4, maximum: 50 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :users, only: [:new, :create, :edit, :update, :destroy] do
+    get("change_password", to: "users#change_password")
+    post('do_reset_password', to: "users#do_change_password")
+    resource :passwords
+  end
+
+  resource :session, only: [:new, :create, :destroy]
+  
 end
